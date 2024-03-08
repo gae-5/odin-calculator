f88147a856932c14fbcd528872c9a1c046190f60
Osur <43894197+gae-5@users.noreply.github.com>
2024-03-08 16:51:04 +0300

    Implement basic math operators

---
+++ js/index.js
@@ -0,0 +1,19 @@
+/*
+ * Basic Math operators on a calculator.
+ */
+function add(firstOperand,secondOperand){
+    return Number(firstOperand)+Number(secondOperand);
+}
+
+function subtract(firstOperand, secondOperand){
+    return Number(firstOperand) - Number(secondOperand);
+}
+
+function multiply(firstOperand, secondOperand){
+    return Number(firstOperand) * Number(secondOperand);
+}
+
+function divide(firstOperand, secondOperand){
+    return Number(firstOperand)/Number(secondOperand)
+}
+
