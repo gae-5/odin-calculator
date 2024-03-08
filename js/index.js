5b147ff873d8028e62bdacd9ee20bb0626fb22bc
Osur <43894197+gae-5@users.noreply.github.com>
2024-03-08 17:08:29 +0300

    Implement operate function

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+let firstOperand = 0;
+let secondOperand = 0;
+let operator = '';
+
+
 /*
  * Basic Math operators on a calculator.
  */
@@ -17,3 +22,21 @@ function divide(firstOperand, secondOperand){
     return Number(firstOperand)/Number(secondOperand)
 }
 
+
+function operate(firstOperand, secondOperand, operator){
+    let result = 0;
+
+    if(operator === '+'){
+       return result = add(firstOperand,secondOperand);
+    } else 
+    if( operator === '-'){
+        return result = subtract(firstOperand, secondOperand);
+    } else 
+    if (operator === '*'){
+        return result = multiply(firstOperand, secondOperand);
+    } else 
+    if( operator === '/'){
+        return result = divide(firstOperand, secondOperand);
+    }
+    return result;
+}
